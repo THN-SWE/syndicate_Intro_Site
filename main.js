688792088e447a017e576816f8d1f1efee6fca5e
thulasikan <thulasikanswe@gmail.com>
2023-08-29 16:25:49 +0530

    safe copy1

---
+++ main.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";function e(e,t){return t.textContent=e,t}function t(e,t){t.appendChild(e)}const n=document.getElementById("content"),c=document.createElement("span"),a=document.createElement("img"),o=document.createElement("p");t(e("Who are Syndicate",c),n),t(a,n),t(e("Syndicate are a team of 7 unatural boys.",o),n)})();
